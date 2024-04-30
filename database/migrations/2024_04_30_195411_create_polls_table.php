805a92b5d16f6f0e3ffb1a0e6e6eb726e1416962
Gabriel Gevert <gevertlolz@gmail.com>
2024-04-30 17:56:01 -0300

    Tabelas e enum do status criados

--- database/migrations/2024_04_30_195411_create_polls_table.php
+++ database/migrations/2024_04_30_195411_create_polls_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\PollStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,10 +14,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('options', function (Blueprint $table) {
+        Schema::create('polls', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('poll_id')->constrained();
-            $table->unsignedBigInteger('votes_count')->default(0);
+            $table->string('title');
+            $table->foreignId('created_by')->constrained('users');
+            $table->dateTime('start_at');
+            $table->dateTime('end_at');
+            $table->enum('status', array_column(PollStatus::cases(), 'value'));
             $table->timestamps();
         });
     }
@@ -28,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('options');
+        Schema::dropIfExists('polls');
     }
 };
